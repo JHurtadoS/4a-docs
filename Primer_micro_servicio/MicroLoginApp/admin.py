025a69bf608947ac877ff980db68de038d21e979
daescawi <93775667+daescawi@users.noreply.github.com>
2021-11-16 16:33:57 -0500

    Update admin

--- Primer_micro_servicio/MicroLoginApp/admin.py
+++ Primer_micro_servicio/MicroLoginApp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models.User import User
+from .models.Propiedad import Propiedad
+from .models.Dueño import Dueño
+from .models.Cliente import Cliente
+from .models.Alquiler import Alquiler
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Propiedad)
+admin.site.register(Dueño)
+admin.site.register(Cliente)
+admin.site.register(Alquiler)
