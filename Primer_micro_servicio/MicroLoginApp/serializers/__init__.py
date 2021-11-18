13ab96fe68e00bdbd009629569dd9798a8833b74
daescawi <93775667+daescawi@users.noreply.github.com>
2021-11-17 21:34:09 -0500

    Update init Serializer

--- Primer_micro_servicio/MicroLoginApp/serializers/__init__.py
+++ Primer_micro_servicio/MicroLoginApp/serializers/__init__.py
@@ -1,1 +1,5 @@
 from .serializerUser import SerializerUser
+from .serializerDueño import SerializerDueño
+from .serializerCliente import SerializerCliente
+from .serializerPropiedad import SerializerPropiedad
+from .serializerAlquiler import SerializerAlquiler
