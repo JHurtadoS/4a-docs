f95dc14bd7f3feb6aac377dbcb347de63e6f6e78
JHurtadoS <juaneshs2014@gmail.com>
2021-11-27 15:30:24 -0500

    formato inicial del segundo micro servicio

---
+++ Segundo_MC/src/test/java/com/grupo_4/Reservacion/ReservacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo_4.Reservacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
