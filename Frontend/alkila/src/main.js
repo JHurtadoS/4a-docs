49ca28f33b9d0e9b62c21ec94431eaa213890e49
unknown <gabrielj.herreraa@gmail.com>
2021-12-09 15:17:58 -0500

    main

--- Frontend/alkila/src/main.js
+++ Frontend/alkila/src/main.js
@@ -1,50 +1,30 @@
-/* eslint-disable prettier/prettier */
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { createApolloProvider } from '@vue/apollo-option'
+import { setContext } from 'apollo-link-context'
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-
-import { faHouseUser } from "@fortawesome/free-solid-svg-icons";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
-
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
-library.add(faHouseUser);
-library.add(faUser);
-
-/*
-
-Vue.component("font-awesome-icon", FontAwesomeIcon);
-*/
-import router from "./router";
-import {
-    ApolloClient,
-    createHttpLink,
-    InMemoryCache,
-} from "@apollo/client/core";
-import { createApolloProvider } from "@vue/apollo-option";
-
-import { setContext } from "apollo-link-context";
 const httpLink = createHttpLink({
-    uri: "https://alkilaapi.herokuapp.com/",
-});
+    uri: 'https://alkilaapi.herokuapp.com/',
+    })
+
 const authLink = setContext((_, { headers }) => {
     return {
         headers: {
             ...headers,
-            Authorization: localStorage.getItem("token_access") || "",
-        },
-    };
-});
+            "Authorization": localStorage.getItem("token_access") || ""
+        }
+    }
+})
+
 const apolloClient = new ApolloClient({
     link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-});
+    cache: new InMemoryCache()
+})
+    
 const apolloProvider = new createApolloProvider({
-    defaultClient: apolloClient,
-});
-createApp(App)
-    .component("font-awesome-icon", FontAwesomeIcon)
-    .use(router)
-    .use(apolloProvider)
-    .mount("#app");
+    defaultClient: apolloClient
+    })
+
+createApp(App).use(router).use(apolloProvider).mount('#app')
