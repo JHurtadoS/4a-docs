b86725e9610a6ab8a17bc00dc34f3ecb837bd591
unknown <gabrielj.herreraa@gmail.com>
2021-12-09 15:08:31 -0500

    app

--- Frontend/alkila/src/App.vue
+++ Frontend/alkila/src/App.vue
@@ -1,30 +1,182 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app" class="app">
+
+    <div class="header">
+
+      <h1> Banco Mision TIC </h1>
+      <nav>
+        <button v-if="is_auth" v-on:click="loadHome"> Inicio </button>
+        <button v-if="is_auth" v-on:click="loadReserva"> Reserva </button>
+        <button v-if="is_auth" v-on:click="loadTransaction"> Transacción </button>
+        <button v-if="is_auth" v-on:click="logOut"> Cerrar Sesión </button>
+        <button v-if="!is_auth" v-on:click="loadLogIn" > Iniciar Sesión </button>
+        <button v-if="!is_auth" v-on:click="loadSignUp" > Reservar </button>
+      </nav>
+    </div>
+    
+
+    <div class="main-component">
+      <router-view  
+        v-on:completedLogIn="completedLogIn"
+        v-on:completedSignUp="completedSignUp"
+        v-on:logOut="logOut"
+      >
+      </router-view>
+    </div>
+    
+
+    <div class="footer">
+      <h2>Misión TIC 2022</h2>
+    </div>
+
   </div>
-  <router-view />
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+export default {
+  name: 'App',
+
+  computed: {
+    is_auth: {
+      get: function() {
+        return this.$route.meta.requieresAuth;
+      },
+      set: function() { }
+    }  
+  },  
+
+  methods:{
+    
+    loadLogIn: function(){
+      this.$router.push({name: "logIn"})
+    },
+
+    loadSignUp: function(){
+      this.$router.push({name: "signUp"})
+    },
+
+    completedLogIn: function(data) {
+			localStorage.setItem("username", data.username);
+			localStorage.setItem("token_access", data.token_access);
+			localStorage.setItem("token_refresh", data.token_refresh);
+			alert("Autenticación Exitosa");
+			this.loadhome();
+    },
+
+    completedSignUp: function(data) {
+			alert("Registro Exitoso");
+			this.completedLogIn(data);
+    },
+
+    loadHome: function() {
+      this.$router.push({ name: "home" });
+    },
+
+    loadReserva: function () {
+			this.$router.push({ name: "reserva" });
+		},
+
+    loadTransaction: function(){
+      this.$router.push({ name: "transaction" });
+    },
+
+    logOut: function () {
+			localStorage.clear();
+			alert("Sesión Cerrada");
+			this.loadLogIn();
+		},
   }
 }
+</script>
+
+
+
+
+
+
+<style>
+
+  body{
+    margin: 0 0 0 0;
+  }
+
+  .header{
+    margin: 0%;
+    padding: 0;
+    width: 100%;
+    height: 10vh; 
+    min-height: 100px;
+
+    background-color: #283747 ;
+    color:#E5E7E9  ;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .header h1{
+    width: 20%;
+    text-align: center;
+  }
+
+  .header nav {
+    height: 100%;
+    width: 30%;
+
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    font-size: 20px;
+  }
+
+  .header nav button{
+    color: #E5E7E9;
+    background: #283747;
+    border: 1px solid #E5E7E9;
+
+    border-radius: 5px;
+    padding: 10px 20px;
+  }
+
+  .header nav button:hover{
+    color: #283747;
+    background: #E5E7E9;
+    border: 1px solid #E5E7E9;
+  }
+
+  
+  .main-component{
+    height: 75vh;
+    margin: 0%;
+    padding: 0%;
+
+    background: #FDFEFE ;
+  }
+
+ 
+  .footer{
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 10vh;
+    min-height: 100px; 
+
+    background-color: #283747;
+    color: #E5E7E9;
+
+  }
+
+  .footer h2{
+    width: 100%;
+    height: 100%;
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
